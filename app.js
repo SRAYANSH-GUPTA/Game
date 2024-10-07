8b1a8d6a565ff24ea1e5020533ff0dbc0706a9d9
srayansh <sr.gupta621@gmail.com>
2024-10-07 23:03:37 +0530

    added image change and moving animation

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
-function change()
+
+document.addEventListener('DOMContentLoaded',()=>
 {
     let i = document.querySelector("#keys");
-let n = 1;
-    while (1)
-        {
-            n = Math.floor(Math.random()*3)
-            i.src = `${n}.jpg`
+    let h = 0;
+    let speeddir = 1; 
+    setInterval(change,1000);
+    setInterval(move,10);
+    let score = 0;
+    function change()
+{
+  
+    let n = 1;
+    
+            n = Math.floor(Math.random()*3);
+            
+            i.src = `data/${n}.jpg`
             console.log(n);
+               
+        
+};
+function move()
+{
+
+    h += speeddir;       
+    i.style.left += h + 'px';
+        if(h + window.width > window.innerWidth)
+        {
+            speeddir = speeddir * -1;   
         }
 }
-Document.addEventListener('DOMContentLoaded',()=>
-{
-    setInterval(change,5000);
-    console.log("now");
+     
 });
