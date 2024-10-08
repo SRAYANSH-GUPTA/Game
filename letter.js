6ea617cd5bd4fdff6e42a3c32ac3fd781c21ce8c
srayansh <sr.gupta621@gmail.com>
2024-10-08 09:57:45 +0530

    Added animaion and game controls

--- letter.js
+++ letter.js
@@ -1,31 +1,56 @@
 let input = []; 
 let h, speeddir, ipos, fpos,letter; 
+let letters = [];
+let classname = 0;
 function generate()
 {
-    let ascii = 21 + Math.floor(Math.random()*40);
+    let ascii = 33 + Math.floor(Math.random()*40);
     input.push(String.fromCharCode(ascii));
+    console.log(ascii);
     letter = document.createElement("div");
     letter.innerHTML = input[input.length-1];
+    letter.className = 'letter' + `${classname}`;
+    classname++;
     console.log(letter);   
-    let y = Math.floor(Math.random()*window.innerWidth);
+    let y = Math.floor(Math.random()*(window.innerWidth-letter.style.width));
+    console.log(window.innerWidth);
+    letter.style.top = ipos + 'px';
     letter.style.left  = y + "px";
     area.appendChild(letter);
-    setInterval(move,100);
+    letters.push({symbol : letter, position : ipos, speeddir : 1,cpos : letter.className});
+    
 };
 function move()
 {
 
-    h += speeddir;       
-    letter.style.top = h + 'px';
-        if(h + letter.offsetHeight >= fpos || h <= ipos)
+   for(let i =0;i < letters.length; i++)
+   {
+    currentletter = letters[i];
+    currentletter.position += currentletter.speeddir;
+    speeddir = currentletter.speeddir;
+    currentletter.symbol.style.top = currentletter.position + "px";
+    if(currentletter.position + currentletter.symbol.offsetHeight >= fpos || currentletter.position <= ipos)
         {
             speeddir = speeddir * -1.2;   
         }
-        else if(Math.abs(speeddir) > 8)
+        else if(Math.abs(speeddir) > 5)
         {
             speeddir /= 1.2;
         }
+        currentletter.speeddir = speeddir;
+   }
+        
 }
+
+document.addEventListener('keydown', (event) => {
+    const button = event.key;
+    console.log(button)
+    const matched = letters.find(letterObj => letterObj.symbol.textContent === button);
+    if (matched) {
+        matched.symbol.remove();
+        letters = letters.filter(letterObj => letterObj !== matched);
+    }
+});
      
 document.addEventListener("DOMContentLoaded",()=>
 {
@@ -37,6 +62,8 @@ document.addEventListener("DOMContentLoaded",()=>
     speeddir = 1; 
     console.log(input);
     setInterval(generate,1000);
+    setInterval(move,100);
+    
     
 
 });
